65bbac30097078ed483034abd0adc9f7425e0a16
𝑹𝒚𝒂𝒏 𝑫𝒂𝒗𝒊𝒅 <ryandultra@gmail.com>
2026-03-11 14:12:20 -0300

    Atualizar o Produtos.jsx

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,15 +1,22 @@
+import produtos from '../data/produtos.json';
+import Card from '../components/Card';
+
 function Produtos() {
-    return (
-      <div className="page-container">
-        <h1>Nossos Produtos</h1>
-        <p>Confira o que podemos te oferecer:</p>
-        <ul>
-          <li>Boneco do TIQUINHO SOARES</li>
-          <li>Pelúcia do CAVALINHO DO FOGÃO</li>
-          <li>Bola autografada pelo goleiro JEFERSSON</li>
-        </ul>
+  return (
+    <div className="container">
+      <h1 style={{ textAlign: 'center', color: '#005a3b', margin: '20px 0' }}>Loja do Fluzão</h1>
+      <div className="grid">
+        {produtos.map((produto) => (
+          <Card
+            key={produto.id}
+            titulo={produto.nome}
+            texto={produto.descricao}
+            imagem={produto.imagem}
+          />
+        ))}
       </div>
-    );
-  }
-  
-  export default Produtos;
+    </div>
+  );
+}
+
+export default Produtos;
