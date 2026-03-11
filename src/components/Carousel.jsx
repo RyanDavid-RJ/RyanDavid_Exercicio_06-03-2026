812786dc28c124304313833ed175c7c3d6eebd74
𝑹𝒚𝒂𝒏 𝑫𝒂𝒗𝒊𝒅 <ryandultra@gmail.com>
2026-03-11 14:06:19 -0300

    Criar Carousel.jsx

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,20 @@
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+function CarouselHome() {
+  return (
+    <Carousel autoPlay infiniteLoop showThumbs={false}>
+      <div>
+        <img src="https://picsum.photos/1200/400?1" alt="Imagem 1" />
+      </div>
+      <div>
+        <img src="https://picsum.photos/1200/400?2" alt="Imagem 2" />
+      </div>
+      <div>
+        <img src="https://picsum.photos/1200/400?3" alt="Imagem 3" />
+      </div>
+    </Carousel>
+  );
+}
+
+export default CarouselHome;
